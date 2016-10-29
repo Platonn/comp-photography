e13623041a115c4e1b972a34e49ba398223bb88a
Platonn <platonn@platonn.com>
2016-10-30 01:28:25 +0200

    Focal stack completed

--- 2.1.py
+++ 2.1.py
@@ -7,10 +7,13 @@ from lib.FocalStack import *
 Timer.start('program')
 
 # read file
-im1 = cv2.imread(LabFiles.input(2, 1, 'stack-1'))
-im2 = cv2.imread(LabFiles.input(2, 1, 'stack-2'))
-im3 = cv2.imread(LabFiles.input(2, 1, 'stack-3'))
+im1 = cv2.imread(LabFiles.input(2, 1, 'stack-1'), cv2.IMREAD_GRAYSCALE)
+im2 = cv2.imread(LabFiles.input(2, 1, 'stack-2'), cv2.IMREAD_GRAYSCALE)
+im3 = cv2.imread(LabFiles.input(2, 1, 'stack-3'), cv2.IMREAD_GRAYSCALE)
 
+cv2.imwrite(LabFiles.output(2, 1, 'stack-1'), im1)
+cv2.imwrite(LabFiles.output(2, 1, 'stack-2'), im2)
+cv2.imwrite(LabFiles.output(2, 1, 'stack-3'), im3)
 # test
 # az = np.array([[[1, 0],
 #                [1, 0]],
@@ -22,22 +25,34 @@ im3 = cv2.imread(LabFiles.input(2, 1, 'stack-3'))
 #
 #        [[2, 1],
 #         [2, 1]]])
+#
+# a = np.array([[1, 1],
+#               [1, 2]])
+#
+# b = np.array([[2, 2],
+#               [2, 2]])
+# focalStack = FocalStack([a, b])
 
-a = np.array([[1, 1],
-              [1, 2]])
+focalStack = FocalStack([im1, im2, im3])
+focalStack.run()
+res = focalStack.getOutput()
+cv2.imwrite(LabFiles.output(2, 1, 'focal-stacked'), res)
+contributions = focalStack.getContributions()
+for i in range(len(contributions)):
+	#print i
+	cv2.imwrite(LabFiles.output(2, 1, 'contribution-' + str(i)), contributions[i])
 
-b = np.array([[2, 2],
-              [2, 2]])
 
+#spike-test ontributuins:
+imTest = cv2.imread(LabFiles.output(2, 1, 'contribution-0'), cv2.IMREAD_GRAYSCALE)
+print imTest
 
-focalStack = FocalStack([a, b])
-res = focalStack.calculateGradients()
-print res
-print res[:,:,0] #maxes
-print res[:,:,1] #indexes maxes
+# print res
+# print res[:,:,0] #maxes
+# print res[:,:,1] #indexes maxes
 
 # stoper
 Timer.stop('program')
 
 # show
-#LabFiles.show(1, 5, 'raw-colors')
+LabFiles.show(2, 1, 'focal-stacked')
--- lib/FocalStack.py
+++ lib/FocalStack.py
@@ -6,29 +6,44 @@ class FocalStack:
 	def __init__(self, images):
 		self.images = images
 
-	def calculateGradients(self):
+	def run(self):
 		imNum = len(self.images)
 		imagesGradients = []
 		for i in range(0, imNum):
+			#print i
 			image = self.images[i]
 			imagesGradients.append(self.__getGradientWithImageIndexChannel(image, i))
 
-		print imagesGradients
+		#print imagesGradients
 		maxArray = imagesGradients[0].copy()  # assume 1st array as maxes
 		for i in range(1, imNum):  # start from 2nd array
+			#print i
 			M = maxArray
 			A = imagesGradients[i]
 			# get 2 arrays: maxes array and maxes indexes array
 			tempArray = np.where(M[:, :, 0] >= A[:, :, 0], [M[:, :, 0], M[:, :, 1]], [A[:, :, 0], A[:, :, 1]])
 			maxArray[:, :, 0] = tempArray[0]
 			maxArray[:, :, 1] = tempArray[1]
-		return maxArray
+		self.maxArray = maxArray
+
+	def getOutput(self):
+		return self.maxArray[:,:,0] #return maxes without indexes
+
+	def getContributions(self):
+		imNum = len(self.images)
+		contributions = []
+		for i in range(imNum):
+			#print i
+			contribution = np.where(self.maxArray[:,:,1] == i, 255, 0) # highlight if it is max
+			contributions.append(contribution)
+		#print contributions
+		return contributions #spike- funkcja nic nie zwraca teraz!
 
 	def __getGradientWithImageIndexChannel(self, image, imageIndex):
 		# spike !!! - teraz nie licza sie gradienty tylko sa brane same obrazki
 		# gradients = GradientCalculator.run(image)
 		gradients = image
-		# print gradients
+		#print gradients[0,0]
 		(height, width) = gradients.shape
 		result = np.zeros((height, width, 2), np.uint8)
 		# print "result"
